03f901327f39a92471d860d7f11bdae5c59520ec
ilovecolors <ilovecolors@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-22 00:22:10 +0000

    git-svn-id: https://plugins.svn.wordpress.org/ilc-flvbox/trunk@88384 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ flvbox.php
@@ -0,0 +1,306 @@
+<?php
+/*
+Plugin Name: ILC FLVBox
+Plugin URI: http://www.ilovecolors.com.ar/
+Description: Shows flv video as inline content or in modal dialog. Uses <a href="http://miplayweb.com/player/" target="_blank">MiPlayWeb</a>, <a href="http://osflv.com/" target="_blank">OSFLV Player</a> and <a href="http://jquery.com/demo/thickbox/" target="_blank">ThickBox</a>.
+Version: 1.0.0
+Author: Elliot
+Author URI: http://www.ilovecolors.com.ar/
+
+ToDo:
+	1) Add separate options pages for players.
+	2) Add complete localization for admin options page
+	3) Add shortcodes.
+	4) Integrate other modal dialogs like nyroModal.
+	5) Add bundled styles for modal dialogs.
+
+*/
+
+/*Global variables*/
+$flvbox_dir = get_bloginfo('url') . get_option('ilc_flvbox_path');
+$flvbox_width = get_option('ilc_flvbox_width');
+$flvbox_height= get_option('ilc_flvbox_height');
+$flvbox_style = dirname(__FILE__) . '/flvbox.css';
+
+/*Save CSS file*/
+if(!empty($_POST['Submit'])){
+	$newStyle = stripslashes($_POST['ilc_flvbox_css']);
+	if(is_writeable($flvbox_style)) {
+	    $fow = fopen($flvbox_style, 'w+');
+	    fwrite($fow, $newStyle);
+		fclose($fow);
+	    echo ' <div id="message" class="updated fade"><p><strong>'. _e('Stylesheet saved.') .'</strong></p></div>'."\n";
+	}
+	else {
+		echo ' <div id="message" class="updated fade"><p><strong>'. _e('There has been an error trying to write the file. Are you sure you have permission to do so?') .'</strong></p></div>'."\n";
+	}
+}
+
+/*-_-_-_-_-_-_-_-_-_-_-_-_ PLUGIN CONTENT PROCESSING _-_-_-_-_-_-_-_-_-_-_-_*/
+
+/*Scripts and styles are added to the head of the rendered page*/
+function ilc_flvbox_wphead() {
+	global $flvbox_dir;
+	global $flvbox_width;
+	global $flvbox_height;
+?>
+	<!-- begin ilc_flvbox scripts -->
+	<?php	if(get_option('ilc_tb') == 'on') { 
+				wp_enqueue_script('jquery');
+				$flvbox_dir_tb = $flvbox_dir;
+				if(get_option('ilc_flvbox_esar') == 'on'){
+					$flvbox_dir_tb .= 'thickbox_ES.js';
+				}
+				else{
+					$flvbox_dir_tb .= 'thickbox_EN.js';
+				}				
+	?>
+				<script type="text/javascript" src="<?php echo $flvbox_dir_tb; ?>"></script>
+				<link rel="stylesheet" href="<?php echo $flvbox_dir; ?>thickbox.css" type="text/css" media="screen" />
+				<link rel="stylesheet" href="<?php echo $flvbox_dir; ?>flvbox.css" type="text/css" media="screen" />
+	<?php	} ?>
+	
+	<script type="text/javascript" src="<?php echo $flvbox_dir; ?>swfobject15.js"></script>
+	<script>
+	//<![CDATA[
+	function ilc_loadVideo(thisVideo)
+	{
+		var ver = "9.0.0";
+		var bgc = "#000000";
+		<?php if (get_option('ilc_player') == 0) { ?>
+			var so = new SWFObject("<?php echo $flvbox_dir; ?>mpwplayer.swf?flv=" + thisVideo + ".flv" + "&jpg=" + thisVideo + ".jpg", thisVideo, "<?php echo $flvbox_width; ?>", "<?php echo $flvbox_height; ?>", ver, bgc, true);
+		<?php }else if (get_option('ilc_player') == 1){
+					if(get_option('ilc_tb') == 'on') { $ilc_flvbox_osflv_ap = 'on';} else { $ilc_flvbox_osflv_ap = 'off';}
+		?>
+			var so = new SWFObject("<?php echo $flvbox_dir; ?>player.swf?movie=" + thisVideo + ".flv&bgcolor=0x<?php echo get_option('ilc_flvbox_osflv_bgcolor'); ?>&fgcolor=0x<?php echo get_option('ilc_flvbox_osflv_fgcolor'); ?>&volume=<?php echo get_option('ilc_flvbox_osflv_volume'); ?>&autoplay=<?php echo $ilc_flvbox_osflv_ap; ?>", thisVideo, "<?php echo $flvbox_width; ?>", "<?php echo $flvbox_height; ?>", ver, bgc, true);
+		<?php } ?>
+		so.addParam("allowfullscreen", "true");
+		so.addParam("wmode", "transparent");
+		so.write(thisVideo);
+	}
+	//]]>
+	</script>
+
+	<!-- end ilc_flvbox scripts -->
+<?php
+}
+/*Adds inline or modal FLV player by filtering the content looking for links to FLV files.*/
+function ilc_flvbox_add($content){
+	
+	global $flvbox_dir;
+	global $flvbox_width;
+	global $flvbox_height;
+	
+	$flvbox_tb_height = $flvbox_height+20;
+	$flvbox_tb_width  = $flvbox_width;
+	
+	$pattern = "/<a(.*?)href=('|\")([^>]*).(flv)('|\")(.*?)>(.*?)<\/a>/i";
+	if(get_option('ilc_tb') == 'on')
+			$replacement = '<div class="ilc_flvbox_inlineimg"><a href="#TB_inline?height=' . $flvbox_tb_height . '&width=' . $flvbox_tb_width . '&inlineId=ilc_flvbox_content" class="thickbox flvbox">
+			<div class="ilc_flvbox_play"></div><img src="$3.jpg" /></a></div>
+			<div id="ilc_flvbox_content" style="display:none">';
+			
+		$replacement .= '<div id="$3" class="flvbox_inline">You need Flash</div><script type="text/javascript">ilc_loadVideo("$3");</script>';
+	
+	if(get_option('ilc_tb') == 'on')
+			$replacement .= '</div>';
+	
+	$content = preg_replace($pattern, $replacement, $content);
+	return $content;
+}
+
+add_action('wp_head', 'ilc_flvbox_wphead');
+add_filter('the_content', 'ilc_flvbox_add');
+
+
+
+/*-_-_-_-_-_-_-_-_-_-_-_-_ PLUGIN ADMIN OPTIONS PAGE _-_-_-_-_-_-_-_-_-_-_-_*/
+/*wp-admin options page*/
+function ilc_flvbox_options(){
+	wp_enqueue_script('jquery');
+	global $flvbox_style;
+	?>
+	
+<script type="text/javascript">
+			jQuery(document).ready(function(){
+				jQuery('#ilc_flvbox_tb').click(function(event){ 
+					jQuery('#ilc_flvbox_css_div').slideToggle('slow');
+				});
+				jQuery('#osflv').click(function(event){ 
+					jQuery('#ilc_flvbox_osflv_div').slideDown('slow');
+				});
+				jQuery("#miplayweb").click(function(event){ 
+					jQuery('#ilc_flvbox_osflv_div').slideUp('slow');
+				});
+			});
+</script>
+	
+<div class="wrap">
+<h2><?php _e('ILC FLVBox Options'); ?></h2>
+
+<form method="post" action="options.php">
+<?php wp_nonce_field('update-options'); ?>
+
+<table class="form-table">
+	<tr valign="top">
+		<th scope="row"><?php _e('Select FLV Player') ?></th>
+		<td>
+			
+			<input id="miplayweb" type="radio" name="ilc_player" value="0" <?php if(get_option('ilc_player') == 0) echo "checked=\"checked\""; ?>/>
+			<label for="miplayweb"><?php _e('Use '); ?><a href="http://miplayweb.com/player/" target="_blank">MiPlayWeb </a></label>
+			<br/>
+			<input id="osflv" type="radio" name="ilc_player" value="1" <?php if(get_option('ilc_player') == 1) echo "checked=\"checked\""; ?>/>
+			<label for="osflv"><?php _e('Use '); ?><a href="http://osflv.com/" target="_blank">OSFLV Player</a></label>
+			<br/>
+			<small><?php _e('Select the player for the flv files.'); ?></small>
+			<div id="ilc_flvbox_osflv_div" <?php if(get_option('ilc_player') != 1) echo "style=\"display:none\""; ?>>
+				<input type="text" name="ilc_flvbox_osflv_bgcolor" size="6" value="<?php echo get_option('ilc_flvbox_osflv_bgcolor'); ?>" />
+				<small><?php _e('OSFLV background elements color.'); ?></small>
+				<br/>
+				<input type="text" name="ilc_flvbox_osflv_fgcolor" size="6" value="<?php echo get_option('ilc_flvbox_osflv_fgcolor'); ?>" />
+				<small><?php _e('OSFLV foreground elements color.'); ?></small>
+				<br/>
+				<input type="text" name="ilc_flvbox_osflv_volume" size="6" value="<?php echo get_option('ilc_flvbox_osflv_volume'); ?>" />
+				<small><?php _e('OSFLV initial volume level.'); ?></small>
+			</div>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row"><?php _e('Use modal dialog') ?></th>
+		<td>
+			<input id="ilc_flvbox_tb" type="checkbox" name="ilc_tb" <?php if(get_option('ilc_tb') == 'on') echo "checked=\"checked\"";?>/>
+			<label for="tb">
+				<?php _e('If you check this option, the video will play on a modal dialog managed by ThickBox.'); ?>
+			</label>
+			
+			<div id="ilc_flvbox_css_div" <?php if(get_option('ilc_tb') != 'on') echo "style=\"display:none\""; ?> >
+				<p><small><?php _e('Set below the CSS style for the image that will be shown');?></small></p>
+				<textarea name="ilc_flvbox_css" cols="60" rows="10"><?php
+					if( is_file($flvbox_style) && filesize($flvbox_style) > 0) {
+						$fo = "";	
+						$fo = fopen($flvbox_style, 'r');
+						$fr = fread($fo, filesize($flvbox_style));
+						echo $fr;
+						fclose($fo);
+					}
+					else _e('The file you\'re looking for can\'t be found.');?>	</textarea>
+			</div>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row"><?php _e('FLVBox dimensions') ?></th>
+		<td>
+			<input type="text" name="ilc_flvbox_width" size="6" value="<?php echo get_option('ilc_flvbox_width'); ?>" />
+			<small><?php _e('Width of the video box.'); ?></small>
+			<input type="text" name="ilc_flvbox_height" size="6" value="<?php echo get_option('ilc_flvbox_height'); ?>" />
+			<small><?php _e('Height of the video box.'); ?></small>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row"><?php _e('Use ThickBox es_AR') ?></th>
+		<td>
+		<input type="checkbox" id="esar" name="ilc_flvbox_esar" <?php if(get_option('ilc_flvbox_esar') == 'on') echo "checked=\"checked\"";?>/>
+			<label for="esar"><?php _e('Check this to use a es_AR translated version of ThickBox'); ?></label>
+		</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row"><?php _e('FLVBox plugin path') ?></th>
+		<td>
+			<input type="text" name="ilc_flvbox_path" size="33" value="<?php echo get_option('ilc_flvbox_path'); ?>" />
+			<p><small><?php _e('Just in case. You shouldn\'t need to change this.'); ?></small></p>
+		</td>
+	</tr>
+
+</table>
+
+<input type="hidden" name="action" value="update" />
+<input type="hidden" name="page_options" value="ilc_flvbox_path,ilc_player,ilc_tb,ilc_flvbox_width,ilc_flvbox_height,ilc_flvbox_esar,ilc_flvbox_osflv_bgcolor,ilc_flvbox_osflv_fgcolor,ilc_flvbox_osflv_volume" />
+
+<p class="submit">
+<input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" class="button-primary" />
+</p>
+
+</form>
+</div>
+
+<?php
+}
+/*Creates Settings link on plugins list page. Taken from Ozh Admin Menu.*/
+function ilc_flvbox_settings($links, $file) {
+	if ($file == plugin_basename(dirname(__FILE__).'/flvbox.php')) {
+		foreach($links as $k=>$v) {
+			if (strpos($v, 'plugin-editor.php?file=') !== false)
+				unset($links[$k]);
+		}
+		$links[] = "<a href='options-general.php?page=ilc_flvbox/flvbox.php'><b>Settings &raquo;</b></a>";
+	}
+	return $links;
+}
+/*Add Settings link on plugins page. Create options page on wp-admin.*/
+function ilc_flvbox_plugin_menu() {
+	add_filter( 'plugin_action_links', 'ilc_flvbox_settings', -10, 2);
+	add_options_page('FLVBox Options', 'FLVBox Options', 8, __FILE__, 'ilc_flvbox_options');
+}
+add_action('admin_menu', 'ilc_flvbox_plugin_menu');
+
+/*When the plugin is activated, we setup some options on the database*/
+function ilc_flvbox_activate(){
+	add_option("ilc_tb", "on");
+	add_option("ilc_flvbox_esar", "");
+	add_option("ilc_player", "0");
+	add_option("ilc_flvbox_width", "427");
+	add_option("ilc_flvbox_height", "320");
+	add_option("ilc_flvbox_path", "/wp-content/plugins/ilc_flvbox/");
+	add_option("ilc_flvbox_osflv_bgcolor", "999999");
+	add_option("ilc_flvbox_osflv_fgcolor", "333333");
+	add_option("ilc_flvbox_osflv_volume", "75");	
+	/*	Just in case you delete the css file
+	.flvbox img{
+		background-color:#CCCCCC;
+		border:5px solid #DDDDDD;
+		padding: 1px;
+	}
+	a.flvbox {
+		background:none !important;
+		border-bottom:none !important;
+	}
+	a.flvbox:hover{
+		border-bottom: 1px dotted #d00 !important;
+	}
+	.ilc_flvbox_inlineimg{
+		margin:0 auto;
+		position:relative;
+		width:50%;
+	}
+	.ilc_flvbox_play{
+		background: url(play.gif) no-repeat top left #000;
+		position: absolute;
+		top: 40%;
+		left: 50%;
+		width: 40px; height:40px;
+	}
+	*/
+}
+/*When the plugin is deactivated, we will erase all options from database*/
+function ilc_flvbox_deactivate(){
+	delete_option("ilc_tb");
+	delete_option("ilc_flvbox_esar");
+	delete_option("ilc_player");
+	delete_option("ilc_flvbox_path");
+	delete_option("ilc_flvbox_width");
+	delete_option("ilc_flvbox_height");
+	delete_option("ilc_flvbox_osflv_bgcolor");
+	delete_option("ilc_flvbox_osflv_fgcolor");
+	delete_option("ilc_flvbox_osflv_volume");
+}
+
+register_activation_hook( __FILE__, 'ilc_flvbox_activate');
+register_deactivation_hook(__FILE__, 'ilc_flvbox_deactivate');
+
+/* Thanks for watching.
+ * If you can donate even USD 1 via the PayPal button at the bottom of my front page I will be very grateful.
+ * Stay tuned on http://ilovecolors.com.ar for more goodies.
+ * */
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== ILC FLVBox ===
+Contributors: ilovecolors
+Donate link: http://ilovecolors.com.ar
+Tags: video, flv, thickbox, modal dialog, admin
+Requires at least: 2.6
+Tested up to: 2.7
+Stable tag: trunk
+
+Plays FLV video inline in content or in a modal dialog.
+
+
+== Description ==
+
+ILC FLVBox will parse your post content looking for links to FLV videos. When found, it will add a player to play it inline or in a modal dialog.
+
+The modal dialog is currently handled by ThickBox, which requires jQuery.
+
+http://jquery.com/demo/thickbox/
+http://jquery.com/
+
+
+The FLV players included are:
+
+http://miplayweb.com/player/
+http://osflv.com/
+
+
+The player embedding uses SWFObject 1.5 (sometimes IE6 doesn't get along nice with SWFObject 2.1 dynamic publishing, as soon as this is sorted out the SWFObject included will be upgraded).
+
+http://blog.deconcept.com/2007/02/28/swfobject-1-5-released/
+http://code.google.com/p/swfobject/ (current 2.1 version)
+
+
+This software is provided without warranty of any kind.
+
+There's an option page with an option to select an FLV Player. You can choose here your player, MiPlayWeb or OSFLV. If you select OSFLV more options are unfolded (background and foreground colors, initial volume level). Flowplayer player will be added later to the list of optional players.
+When you check the box enabling ThickBox, a textarea is unfolded containing an CSS file that will allow you to set the style of the inline image. You can also edit this file independently on your editor of choice, by opening the flvbox.css file.
+The FLVBox dimensions option sets the size of the player (indirectly, the video, but not specifically). The ThickBox area is adjusted accordingly.
+You can check the corresponding checkbox to use an spanish version of ThickBox. The english version ThickBox is modified with additional effects such as fadeIn, slideDown, fadeOut, etc.
+There's one last field in case you need to rename the folder or change the path of ILC_FLVBox.
+
+== Installation ==
+
+1. Upload `ilc_flvbox` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. You can access the options page via the Admin navigation or in the plugins list, using the Settings link.
+4. Edit a post/page, upload a video using the Add Video button and insert the link to the file into the post.
+
+
+== Frequently Asked Questions ==
+
+= I have changed ThickBox's css and nothing happens =
+Make sure you don't have other ThickBox loading.
+
+= Which is the version of jQuery that this plugin uses? =
+The plugin enqueues the jQuery version included in WordPress. As of version 2.7 WP includes jQuery version 1.2.6. But WP 2.3.2 includes version 1.1.4 so it will probably be ok. I haven't tested ThickBox with jQuery 1.3 yet.
+
+== Screenshots ==
+
+1. Options page in WP 2.7
+2. Inline video when not using ThickBox.
+3. Inline image when using ThickBox (play icon automatically added).
+4. Video in ThickBox.
+
+
